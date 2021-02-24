4391df5d7787d9b03fd3785b12526730be3dc5d2
valeriagrazzini <v.grazzini@hyperting.com>
2021-02-24 15:47:25 +0100

    test subscription with filters

--- src/api/message.ts
+++ src/api/message.ts
@@ -60,8 +60,9 @@ export class MessageResolver {
 
   @Subscription(() => Message, {
     topics: topic,
+    filter: ({ payload, args }) => (args.chatId = payload.chatId),
   })
-  async newMessage(@Root() message: Message): Promise<Message> {
+  async newMessage(@Root() message: Message, @Arg('chatId', () => ID) chatId: number): Promise<Message> {
     return {
       ...message,
     }
